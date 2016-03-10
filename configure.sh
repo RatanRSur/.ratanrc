57f5259961707e07c64605fc8097aed9d00fccb7
Ratan <ratan.r.sur@gmail.com>
2016-03-09 21:26:59 -0500

    config script changes [wip]

--- configure.sh
+++ configure.sh
@@ -1,4 +1,19 @@
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+which xmodmap && ln .Xmodmap ~/.Xmodmap
+which i3 && {
+mkdir -p ~/.config/i3
+ln .config/i3 ~/.config/i3
+}
+which tmux && ln .tmux.conf ~/.tmux.conf
+which zsh && ln .zshrc ~/zshrc
+
 cd ~
-vim +PluginInstall
-mkdir .vim/undodir
+if which nvim
+then
+    plug_dir="~/.config/nvim/autoload"
+    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs $plug_dir
+    nvim +PlugInstall
+else
+    plug_dir="~/.vim/autoload"
+    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs $plug_dir
+    vim +PlugInstall
+fi
