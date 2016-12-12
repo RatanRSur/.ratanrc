268b6b3dbc1db60ed3fac55c2a93231c78bedf50
Ubuntu <ratan@servtan.rj1ezo53f4zelmtunuj2wvg1ze.bx.internal.cloudapp.net>
2016-12-12 20:06:00 +0000

    configure: url for plug

--- configure.sh
+++ configure.sh
@@ -10,7 +10,7 @@ cd ~
 if which nvim
 then
     plug_dir="~/.config/nvim/autoload"
-    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs $plug_dir
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     nvim +PlugInstall
 else
     plug_dir="~/.vim/autoload"
